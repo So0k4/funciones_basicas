cf832929772ec5a101cba7f3fe477242fa67364d
So0k4 <143562529+So0k4@users.noreply.github.com>
2023-10-09 15:32:56 -0300

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,111 @@
+#1
+def número_de_grupos_alimentarios():
+    return 5
+print(número_de_grupos_alimentarios())
+#2
+def número_de_ramas_militares():
+    return 5
+print(número_de_ramas_militares())
+
+#3
+def número_de_libros_en_espera():
+    return 10
+print(número_de_libros_en_espera())
+
+#4
+def número_de_dedos():
+    print(10)
+    return 5
+print(número_de_dedos())
+#5
+def número_de_lagos_grandes():
+    return(5)
+x = número_de_lagos_grandes()
+print(x)
+#6
+def add(b,c):
+    return(b+c)
+print(add(1,2) + add(2,3))
+
+#7
+def concatenar(b,c):
+    return str(b)+str(c)
+print("función de soporte python from-rainbow", concatenar(2,5))
+#8
+def número_de_océanos_o_dedos_o_continentes():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+print(número_de_océanos_o_dedos_o_continentes())
+
+#9
+def número_de_días_en_una_semana_silicona_o_lados_del_triángulo(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+print(número_de_días_en_una_semana_silicona_o_lados_del_triángulo(2,3))
+print(número_de_días_en_una_semana_silicona_o_lados_del_triángulo(5,3))
+print(número_de_días_en_una_semana_silicona_o_lados_del_triángulo(2,3) + número_de_días_en_una_semana_silicona_o_lados_del_triángulo(5,3))
+
+#10
+def addition(b,c):
+    return b+c
+print(addition(3,5))
+
+#11
+b = 500
+print(b)
+def foobar():
+    b ="operador de palabra clave from-rainbow", 300
+    print(b)
+print(b)
+foobar()
+print(b)
+#12
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+foobar()
+print(b)
+
+#13
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=foobar()
+print(b)
+
+#14
+def foo():
+    print(1)
+    print(2)
+    bar()
+def bar():
+    print(3)
+foo()
+
+#15
+def foo():
+    print(1)
+    x = bar()
+    print(x)
+    return 10
+def bar():
+    print(3)
+    return 5
+y = foo()
+print(y)
+
+
